7761dab92ce1b3ab6881d813e85cc6365d14a8dd
leavez <gaojiji@gmail.com>
2017-07-14 02:01:26 +0800

    demo

---
+++ AlternateIconDemo/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  AlternateIconDemo
+//
+//  Created by Gao on 13/07/2017.
+//  Copyright © 2017 leave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func didTapDefault() {
+        setIcon(name: nil)
+    }
+
+    @IBAction func didTapIcon1() {
+        setIcon(name: "icon1")
+    }
+
+    @IBAction func didTapIcon2() {
+        setIcon(name: "icon2")
+    }
+
+    func setIcon(name:String?) {
+        UIApplication.shared.setAlternateIconName(name) { (error) in
+            if let error = error {
+                print(error)
+            }
+        }
+    }
+}
+
